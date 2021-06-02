a775f37f9f6783478d1a30c0fb0135fd893d5514
Alireza Sharifpour <your_email_address@example.com>
2021-06-02 20:49:20 +0430

    Testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import "../styles/globals.css";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fafa",
+    },
+    secondary: {
+      main: "#4Fad",
+    },
+  },
+  direction: "rtl",
+});
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [rtlPlugin],
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div dir="rtl">
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
